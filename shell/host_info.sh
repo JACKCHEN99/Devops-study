edb5455ebc41d671c76be84b8eae2dcd3ef6b77a
JACKCHEN99 <wudiudiu99@gmail.com>
2020-12-31 00:40:01 +0800

    Update host_info.sh

--- shell/host_info.sh
+++ shell/host_info.sh
@@ -3,7 +3,6 @@ cpu_rem=`vmstat | awk 'NR>2{print $(NF-2)}'`
 mem_rem=`free -h | awk 'NR==2{print $NF}'`
 swap=`free -h | awk 'NR==3{print $2" "$3}'`
 disk_used=`df -h | awk '/\/$/{print $(NF-1)}'`
-
 echo "cpu free: $cpu_rem"
 echo "free remain: $mem_rem"
 echo "disk used: $disk_used"
