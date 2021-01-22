31541627e0bc2c97f373292b99c7cea45cdc04e5
JACKCHEN99 <wudiudiu99@gmail.com>
2021-01-22 14:41:03 +0800

    服务健康检查,状态为0的检查脚本

---
+++ shell/check_health_pod.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+>result.txt
+
+chk_health() {
+    while read line;do
+        kubectl get pods -n $1 | grep $line &>/dev/null
+        if [[ $? -eq 0 ]]; then
+            pod_name=`kubectl get pods -n $1 | grep $line | awk '{print $1}'`
+            pod_status=`kubectl get pods -n $1 | grep $line | awk '{print $3}'`
+            kubectl get svc -n $1 | grep $line &>/dev/null
+            [ $? -eq 0 ] && echo "$pod_name $pod_status svc_yes" >> result.txt || echo "$pod_name $pod_status svc_no" >> result.txt
+        else
+            ls /data/script/k8s-uatstable/yaml/$line &>/dev/null
+            [ $? -eq 0 ]  && echo "$line not_found no_yaml" >> result.txt || echo "$line no_found yaml_yes no_yaml" >> result.txt
+        fi
+    done< $1.txt
+    echo
+    echo "$1 brand pod check result:"
+    echo "++++++++++++++++exist++++++++++++++++++"
+    cat result.txt | awk '/svc/{print $1,$2,$3}' | column -t
+    echo "+++++++++++++++++not exist++++++++++++++++++"
+    #cat result.txt | grep 'no_found'
+    cat result.txt | awk '/no_found/{print $1,$3}' | column -t
+}
+
+chk_health $1
