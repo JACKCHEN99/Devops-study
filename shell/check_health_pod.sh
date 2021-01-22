e5b4916bc7697e94571c6f0a9ed259f7c8765d12
JACKCHEN99 <wudiudiu99@gmail.com>
2021-01-22 16:16:57 +0800

    check_health_pod.sh update

--- shell/check_health_pod.sh
+++ shell/check_health_pod.sh
@@ -10,8 +10,8 @@ chk_health() {
             kubectl get svc -n $1 | grep $line &>/dev/null
             [ $? -eq 0 ] && echo "$pod_name $pod_status svc_yes" >> result.txt || echo "$pod_name $pod_status svc_no" >> result.txt
         else
-            ls /data/script/k8s-uatstable/yaml/$line &>/dev/null
-            [ $? -eq 0 ]  && echo "$line not_found no_yaml" >> result.txt || echo "$line no_found yaml_yes no_yaml" >> result.txt
+            ls /data/script/k8s-uatstable/yaml/${line}.yaml &>/dev/null
+            [ $? -eq 0 ]  && echo "$line not_found yes_yaml" >> result.txt || echo "$line not_found no_yaml" >>result.txt
         fi
     done< $1.txt
     echo
@@ -20,7 +20,8 @@ chk_health() {
     cat result.txt | awk '/svc/{print $1,$2,$3}' | column -t
     echo "+++++++++++++++++not exist++++++++++++++++++"
     #cat result.txt | grep 'no_found'
-    cat result.txt | awk '/no_found/{print $1,$3}' | column -t
+    cat result.txt | awk '/not_found/{print $1,$3}' | sort -rk 2 | column -t
+    echo
 }
 
 chk_health $1
